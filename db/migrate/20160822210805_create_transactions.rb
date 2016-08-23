3be00232777318fe98b7021caeaef390e40b5de4
Andrew Furth <andrew.furth@gmail.com>
2016-08-23 10:43:22 -0700

    migration modified; all rows uncommented

--- db/migrate/20160822210805_create_transactions.rb
+++ db/migrate/20160822210805_create_transactions.rb
@@ -1,7 +1,7 @@
 class CreateTransactions < ActiveRecord::Migration
   def self.up 
     create_table :transactions do |t|
-      # t.datetime :transaction_date
+      t.datetime :transaction_date
       t.string :product
       t.integer :price
       t.string :payment_type
@@ -9,8 +9,8 @@ class CreateTransactions < ActiveRecord::Migration
       t.string :city
       t.string :state
       t.string :country
-      # t.datetime :account_created
-      # t.datetime :last_login
+      t.datetime :account_created
+      t.datetime :last_login
       t.timestamps
     end
   end
